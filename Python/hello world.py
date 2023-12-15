9b8ac4196860f0c20f5d462bbb32ad891b50b09c
Jhony Andreas <Jhony.Andreas@bni.co.id>
2023-12-15 09:45:24 +0700

    dev pass

--- Python/hello world.py
+++ Python/hello world.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-print("hello world") #12345
-print("new_line") 
-=======
-print("hello world") #dari branch12345
-<<<<<<< HEAD
-print("new_line") #new code1
-=======
-print("new_line") #new code
->>>>>>> 49afb9756507b6aa642dfc3d3100875153dc96ae
->>>>>>> parent of e124105 (request1)
+print("dev pass")
+
